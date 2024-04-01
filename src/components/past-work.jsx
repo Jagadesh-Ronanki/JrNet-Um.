fdd37872b3172fff6d99505c7d0a409d0d309175
Jagadesh <jagadesh.ronanki@gmail.com>
2024-04-01 12:30:35 +0530

    bug:button-w-smallscreens

--- src/components/past-work.jsx
+++ src/components/past-work.jsx
@@ -32,7 +32,7 @@ const PastWork = () => {
                   />
                   <span className="font-mono text-[16px]">{audit.title}</span>
                 </div>
-                <Link href={audit.slug}>
+                <Link href={audit.slug} className="max-md:w-full max-md:mt-2">
                   <Button  className="rounded-full border-[0.4px] text-white border-stone-700 bg-black hover:bg-neutral-900/40 max-md:w-full max-md:mt-2 font-extralight tracking-wider" variant="secondary">
                     Read Report
                     <ArrowUpRight className="pl-2 opacity-90" />
